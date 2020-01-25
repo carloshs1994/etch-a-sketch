c0d873a57b8cc0730b4a74edcca692ff9a1eaf99
Carlos Herver <carlos.h.s.1994@gmail.com>
2020-01-25 00:34:05 -0600

    base project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # etch-a-sketch
+This is a repository made for The odin project
---
+++ etch-a-sketch.js
@@ -0,0 +1,51 @@
+const container = document.querySelector('#container');
+
+const button = document.querySelector('#button');
+button.addEventListener("click", clearAndResize);
+
+function createGrid(lateralSize){
+  for (var i = 0; i < lateralSize**2; i++) {
+    let content = document.createElement('div');
+    content.classList.add('content');
+    container.appendChild(content);
+  }
+  if (lateralSize) {
+    container.style.gridTemplateColumns = "repeat(" + lateralSize + ",1fr)";
+  }
+  const box = document.querySelectorAll('div.content');
+  box.forEach((box) => {
+    box.addEventListener('mouseover', changeColor);
+  });
+  let changeLight = 0;
+  let decreasingLigth = 0;
+  function changeColor(e) {
+    e.target.style.background = "rgb(" + defaultRandomColor(changeLight,changeLight,changeLight) + ")";
+    if ((changeLight < 255 && decreasingLigth == 0) || changeLight == 0){
+      changeLight ++;
+      decreasingLigth = 0;
+    } else {
+      changeLight --;
+      decreasingLigth = 1;
+    }
+    console.log(changeLight);
+  }
+}
+
+createGrid(16);
+
+function clearAndResize() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  let gridLateralSize = prompt("How many squares do you want? from 1 to 100 ");
+  while (gridLateralSize < 1 || gridLateralSize > 100) {
+    gridLateralSize = prompt("How many squares do you want? from 1 to 100 ");
+  }
+    createGrid(gridLateralSize);
+}
+
+function defaultRandomColor(randomR = Math.floor(Math.random() * 255),
+                            randomG = Math.floor(Math.random() * 255),
+                            randomB = Math.floor(Math.random() * 255)) {
+  return randomR + "," + randomG + "," + randomB;
+}
